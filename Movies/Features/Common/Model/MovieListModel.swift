db86d98772feb1988cbd833113a88be5a89f5803
Balazs Kanyo <bkanyo@gmail.com>
2023-10-24 08:32:40 +0200

    HW-2: create use case

---
+++ Movies/Features/Common/Model/MovieListModel.swift
@@ -0,0 +1,13 @@
+//
+//  MovieListModel.swift
+//  Movies
+//
+//  Created by Balazs Kanyo on 23/10/2023.
+//
+
+import Foundation
+
+struct MovieListModel {
+    let movies: [MovieVM]
+    let maxPopularity: Float
+}
---
+++ Movies/Features/Common/UseCase/GetTrendingMoviesUseCase.swift
@@ -0,0 +1,36 @@
+//
+//  GetTrendingMoviesUseCase.swift
+//  Movies
+//
+//  Created by Balazs Kanyo on 23/10/2023.
+//
+
+import Combine
+import Foundation
+
+protocol GetTrendingMoviesUseCase {
+    func execute() -> AnyPublisher<DataTask<MovieListModel, ApiError>, Never>
+}
+
+final class GetTrendingMoviesUseCaseImpl {
+    private let repository: MovieRepository
+
+    init(repository: MovieRepository) {
+        self.repository = repository
+    }
+}
+
+extension GetTrendingMoviesUseCaseImpl: GetTrendingMoviesUseCase {
+    func execute() -> AnyPublisher<DataTask<MovieListModel, ApiError>, Never> {
+        repository
+            .getTrending()
+            .mapLoaded { movieList in
+                return .init(
+                    movies: movieList,
+                    maxPopularity: movieList
+                        .map { $0.popularity }
+                        .reduce(0, { $0 > $1 ? $0 : $1 })
+                )
+            }
+    }
+}
